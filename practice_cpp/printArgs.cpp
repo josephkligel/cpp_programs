877db4c92383022e8d6450c0e3450fd251ea2d81
Joseph Kligel <jkligel@gmail.com>
2020-06-21 05:48:02 -0500

    Using bash script to push all updates

---
+++ practice_cpp/printArgs.cpp
@@ -0,0 +1,7 @@
+#include <iostream>
+
+int main(int argc, char *argv[]){
+	std::cout << "There are " << argc << " arguments.\n";
+	std::cout << "This is the first argument " << argv[1] << "\n";
+	return 0;
+}
