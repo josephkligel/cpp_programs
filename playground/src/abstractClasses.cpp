422a02268ca5066c0a1c2cc16db961320aaf2d73
Joseph Kligel <jkligel@gmail.com>
2020-09-15 23:16:06 -0500

    gitpy to your rescue

--- playground/src/abstractClasses.cpp
+++ playground/src/abstractClasses.cpp
@@ -1,11 +1,12 @@
 #include <iostream>
+#include <vector>
 using namespace std;
 
 class Shape{ //Abstract
     public:
         virtual void draw() = 0;
         virtual void rotate() = 0;
-        virtual ~Shape();
+        virtual ~Shape(){}
 };
 
 class Open_Shape: public Shape{ //Abstract
@@ -29,20 +30,42 @@ class Line: public Open_Shape{ //Concrete
         virtual ~Line(){}
 };
 
-class Circle: public Closed_Shape{
+class Circle: public Closed_Shape{ //Concrete
     public:
         virtual void draw() override{
             std::cout << "Drawing a circle" << std::endl;
         }
         virtual void rotate() override{
-            std::cout << "Drawing a circle" << std::endl;
+            std::cout << "Rotating a circle" << std::endl;
         }
-        virtual ~Circle();
+        virtual ~Circle(){}
 };
 
+void screen_refresh(const std::vector<Shape *> &shapes){
+    std::cout << "Refreshing" << std::endl;
+    for(const auto p: shapes)
+        p->draw();
+}
+
 int main(){
-    Circle c {};
-    c.draw();
+    // Circle c;
+    // c.draw();
+
+    // Shape *ptr = new Circle();
+    // ptr->draw();
+    // ptr->rotate();
+
+    Shape *s1 = new Circle();
+    Shape *s2 = new Line();
+
+    std::vector<Shape *>
+    shapes {s1, s2};
+    // for(const auto p: shapes)
+    //     p->draw();
+    screen_refresh(shapes);
+
+    delete s1;
+    delete s2;
 
     return 0;
 }
--- playground/src/printArgs.cpp
+++ playground/src/printArgs.cpp
@@ -1,7 +1,9 @@
 #include <iostream>
 
 int main(int argc, char *argv[]){
-	std::cout << "There are " << argc << " arguments.\n";
-	std::cout << "This is the first argument " << argv[1] << "\n";
+	std::cout << argc << " args were given"  << std::endl;
+	for(size_t i{}; i < sizeof(*argv); i++){
+		std::cout << i + 1 << ") " << argv[i] << std::endl;
+	}
 	return 0;
 }
